6b18fa364e68e8b19f0b59567c727e46b4d4d4d4
UnsignedByte <edlam1889@gmail.com>
2020-12-16 23:28:13 -0800

    updatum

--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@
 # @Author: UnsignedByte
 # @Date:   09:39:42, 04-Dec-2020
 # @Last Modified by:   UnsignedByte
-# @Last Modified time: 20:09:57, 13-Dec-2020
+# @Last Modified time: 23:01:05, 16-Dec-2020
 
 import numpy as np
 import utils
@@ -31,6 +31,7 @@ for name in names:
 
 	netCount, gamesPer, fakeAgents, gameCount, generations, P, M, G, brainShape = np.load(os.path.join(fpath, 'params.npy'), allow_pickle=True)
 	print(f'Brain shape was {brainShape}, Memory length {int(brainShape[0]/M/P)}')
+	print(netCount, gamesPer, fakeAgents, gameCount, generations)
 
 	percentiles = [0, 10, 25, 50, 75, 90, 100]
 	percstyles = ['-','--', ':', '-', ':', '--', '-']
@@ -39,19 +40,24 @@ for name in names:
 	substitutions = 200; # number of generations to read out of total
 
 	scorePercentiles = np.zeros((substitutions,len(percentiles)));
-	rcountPercentiles = np.zeros((substitutions,M,len(percentiles)))
+	rcountPercentiles = np.zeros((substitutions,M-1,len(percentiles)))
 
 	grange = range(0, generations, int(generations/substitutions));
 
 	for i in range(substitutions):
 		nets = sorted(np.load(os.path.join(fpath, 'raws', f'gen_{grange[i]}.npy'), allow_pickle=True), key=lambda x:x.score/x.plays)
 		scorePercentiles[i]=np.percentile([x.score/x.plays for x in nets], percentiles);
-		# rcountPercentiles[i]=np.apply_along_axis(lambda x:np.percentile(x, percentiles), 1, np.transpose([(lambda k:[k[0]]+[k[i-1]+k[i]for i in range(1,len(k))])(x.rcount/x.plays)[:-1] for x in nets]))
-		rcountPercentiles[i]=np.apply_along_axis(lambda x:np.percentile(x, percentiles), 1, np.transpose([x.rcount/x.plays for x in nets]))
+		rcountPercentiles[i]=np.apply_along_axis(lambda x:np.percentile(x, percentiles), 1, np.transpose([(lambda k:[k[0]]+[k[i-1]+k[i]for i in range(1,len(k))])(x.rcount/x.plays)[:-1] for x in nets]))
+		# rcountPercentiles[i]=np.apply_along_axis(lambda x:np.percentile(x, percentiles), 1, np.transpose([x.rcount/x.plays for x in nets]))
 
 		if i==substitutions-1:
 			res = brain.runGame([nets[-1]]*P, 100, M, G);
-			# print(res[3])
+			print(res[3])
+			if os.path.isfile(os.path.join(fpath, 'test.txt')):
+				with open(os.path.join(fpath, 'test.txt')) as f:
+					for x in f.readlines():
+						print(list(map(int, x.split())))
+						brain.testCase(nets[-1], list(map(int, x.split())))
 
 
 		if i%100==0:
@@ -72,11 +78,11 @@ for name in names:
 
 	rcountAx = fig.add_subplot(*plotgrid, 2, sharex = scoreAx);
 	for i in range(len(percentiles)):
-		for j in range(M):
+		for j in range(M-1):
 			rcountAx.plot(grange, rcountPercentiles[:,j,i], label=f'{percentiles[i]}%', linestyle=percstyles[i], linewidth=percwidth[i], color=cmap(j))
 	scoreAx.get_legend_handles_labels()
 	l1 = rcountAx.legend(*scoreAx.get_legend_handles_labels(), bbox_to_anchor=(1.05, 0, 0.25, 1), loc='upper left', borderaxespad=0.)
-	rcountAx.legend(handles=[matplotlib.patches.Patch(color=cmap(i), label=f'Move {i+1}') for i in range(M)], bbox_to_anchor=(1.05, 0, 0.25, 1), loc='lower left', borderaxespad=0.)
+	rcountAx.legend(handles=[matplotlib.patches.Patch(color=cmap(i), label=f'Move {i+1}') for i in range(M-1)], bbox_to_anchor=(1.05, 0, 0.25, 1), loc='lower left', borderaxespad=0.)
 	rcountAx.add_artist(l1)
 	rcountAx.set_title('Average proportions of each move')
 	rcountAx.set_ylabel('Proportion')
@@ -84,13 +90,4 @@ for name in names:
 	rcountAx.set_ylim(0, 1)
 
 	plt.savefig(os.path.join(fpath, 'scores.svg'), bbox_inches='tight')
-
-	# print(f"\n{bcolors.HEADER}Best brain for generation {generations-1}{bcolors.ENDC}")
-	# n = nets[np.argmax(np.vectorize(lambda x:x.score/x.plays)(nets))]
-	# print(sum([i.rcount for i in nets]))
-	# print(f'{bcolors.OKGREEN}Average score per game: {n.score/n.plays}{bcolors.ENDC}')
-	# print(f'Number of each choice: {bcolors.FAIL}{n.rcount}{bcolors.ENDC}')
-
-	# for m in brain.cases:
-	# 	if len(m) == brainShape[0]/M:
-	# 		brain.testCase(n, m)
+	plt.savefig(os.path.join(fpath, 'scores.png'), bbox_inches='tight')
